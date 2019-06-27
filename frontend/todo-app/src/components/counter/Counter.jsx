7be5f51f9bad9cbd3ed6ef0665e835ed70c1e9c8
divukman <divukman@gmail.com>
2019-06-27 12:55:07 +0200

    Components.

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -11,24 +11,61 @@ class Counter extends Component {
     };
 
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   render() {
     return (
       <div className="Counter">
-        <CounterButton incrementMethod={this.increment} />
-        <CounterButton by={5} incrementMethod={this.increment} />
-        <CounterButton by={10} incrementMethod={this.increment} />
+        <CounterButton
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        {
+          // Moga je ovako to risit: <CounterButton by={-1} incrementMethod={this.increment} />
+        }
+        <CounterButton
+          by={5}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        <CounterButton
+          by={10}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+
         <span className="count"> {this.state.counter} </span>
+
+        <div>
+          <button className="reset" onClick={this.reset}>
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
 
   // Ovaj nacin se cini pogresan. Mozda bi bilo idealnije sa Observer Patternom rjesiti.
+  // Samo, kako ces u java scriptu rec da je nesto interface, pa proslijedit... dobro, moze se nesto slicno
   increment(by) {
     console.log(`Increment from child: {by}`);
-    this.setState({
-      counter: this.state.counter + by
+    this.setState(prevState => {
+      return { counter: prevState.counter + by };
+    });
+  }
+
+  decrement(by) {
+    console.log(`Decrement from child: {by}`);
+    this.setState(prevState => {
+      return { counter: prevState.counter - by };
+    });
+  }
+
+  reset() {
+    this.setState(() => {
+      return { counter: 0 };
     });
   }
 }
@@ -36,31 +73,24 @@ class Counter extends Component {
 class CounterButton extends Component {
   constructor() {
     super();
-
-    this.state = {
-      counter: 0
-    };
-
-    this.increment = this.increment.bind(this);
   }
 
+  /**
+   * Pazi jako dobro doli. onClicke ocekuje REFERENCU, ne mozes samo pozvat metodu. Zato arrow metoda.
+   */
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}> + {this.props.by} </button>
-        {
-          //<span className="count"> {this.state.counter} </span>
-        }
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>
+          {" "}
+          + {this.props.by}{" "}
+        </button>
+        <button onClick={() => this.props.decrementMethod(this.props.by)}>
+          {" "}
+          - {this.props.by}{" "}
+        </button>
       </div>
     );
-  }
-
-  increment() {
-    this.setState({
-      counter: this.state.counter + this.props.by
-    });
-
-    this.props.incrementMethod(this.props.by);
   }
 }
 
@@ -70,7 +100,8 @@ CounterButton.defaultProps = {
 
 CounterButton.propTypes = {
   by: PropTypes.number,
-  incrementMethod: PropTypes.func
+  incrementMethod: PropTypes.func,
+  decrementMethod: PropTypes.func
 };
 
 export default Counter;
