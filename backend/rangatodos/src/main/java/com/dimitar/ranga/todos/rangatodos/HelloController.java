0bc61a4e49aa5e3466caa3cf9ffc86cae8d4cfd5
divukman <divukman@gmail.com>
2019-06-29 17:18:16 +0200

    backend work, play

--- backend/rangatodos/src/main/java/com/dimitar/ranga/todos/rangatodos/HelloController.java
+++ backend/rangatodos/src/main/java/com/dimitar/ranga/todos/rangatodos/HelloController.java
@@ -3,6 +3,7 @@ package com.dimitar.ranga.todos.rangatodos;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,16 @@ public class HelloController {
     @GetMapping("/hello-world")
     public ResponseEntity<?> helloWorld() {
         return new ResponseEntity<String>("Hello World with Ranga ;)", HttpStatus.OK);
+    }
+
+    @GetMapping("/hello-world-bean")
+    public ResponseEntity<?> helloWorldBean() {
+        return new ResponseEntity<HelloBean>(new HelloBean("Hello Ranga from the Bean!"), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/hello-world/{name}")
+    public ResponseEntity<?> helloWorldBeanPath(@PathVariable final String name) {
+        return new ResponseEntity<HelloBean>(new HelloBean(String.format("Hello World and hello %s!", name)), HttpStatus.OK);
     }
 }
